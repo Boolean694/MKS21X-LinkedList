1cbaa126fae7da91ac6c1bf9e4face2f7f331567
Julian Wong <jwong01@stuy.edu>
2018-12-12 23:35:20 -0500

    write stuff yet to test

--- LinkedList.java
+++ LinkedList.java
@@ -2,4 +2,28 @@ public class LinkedList {
 	private int size;
 	private Node start;
 	private Node end;
+	public int size() {
+		int g = 2;
+		Node c = start.gn();
+		while(c.gn() != null) {
+			g++;
+			c = c.gn();
+		}
+		return g;
+	}
+	public boolean add(int v) {
+		Node nn = new Node(v, null, end);
+		end = nn;
+	}
+	public String toString() {
+		String s = "[" + start.gd() + ",";
+		Node c = start.gn();
+		while(c.gn() != null) {
+			s += c.gd();
+			s += ",";
+		}
+		s += end.gd();
+		s += "]"
+		return s;
+	}
 }
--- Node.java
+++ Node.java
@@ -2,4 +2,18 @@ public class Node {
 	private int data;
 	private Node next;
 	private Node prev;
+	public Node(int d, Node n, Node p) {
+		data = d;
+		next = n;
+		prev = p;
+	}
+	public int gd() {
+		return data;
+	}
+	public Node gn() {
+		return next;
+	}
+	public Node gp() {
+		return prev;
+	}
 }
